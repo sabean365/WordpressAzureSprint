506f08c1ca7a8664e83d6220ed2d2ddb3f360cfc
lucidlactose <jesusvillarruel98@gmail.com>
2022-06-03 12:15:44 -0700

    adding a working load balancer

--- terraform-manifest/c9-02-web-loadbalancer-resource.tf
+++ terraform-manifest/c9-02-web-loadbalancer-resource.tf
@@ -42,7 +42,7 @@ resource "azurerm_lb_rule" "web_lb_rule_app1" {
   frontend_port                  = 80
   backend_port                   = 80
   frontend_ip_configuration_name = azurerm_lb.web_lb.frontend_ip_configuration[0].name
-  # backend_address_pool_id        = azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id 
+  backend_address_pool_ids        = [azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id]
   probe_id                       = azurerm_lb_probe.web_lb_probe.id
   loadbalancer_id                = azurerm_lb.web_lb.id
   # resource_group_name            = azurerm_resource_group.rg.name
