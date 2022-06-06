dcf0c9c202cd7d942d835213ef61b43f9f4452eb
Yaimara Narito <85261784+YaimaraNarito@users.noreply.github.com>
2022-06-06 09:46:51 -0500

    Updated terraform_manifest

--- terraform-manifest/c10-01-mysql-servers-input-variables.tf
+++ terraform-manifest/c10-01-mysql-servers-input-variables.tf
@@ -3,22 +3,26 @@
 variable "mysql_db_name" {
   description = "Azure MySQL Database Name"
   type        = string
+  default     =  "scrumsquad-mysql"
 }
 
 # DB Username - Enable Sensitive flag
 variable "mysql_db_username" {
   description = "Azure MySQL Database Administrator Username"
   type        = string
+  default     = "scrumsquad-mysql"
 }
 # DB Password - Enable Sensitive flag
 variable "mysql_db_password" {
   description = "Azure MySQL Database Administrator Password"
   type        = string
   sensitive   = true
+  default     = "@ScraumSquad!%5*&SDJRY"
 }
 
 # DB Schema Name
 variable "mysql_db_schema" {
   description = "Azure MySQL Database Schema Name"
   type        = string
+  default     = "scrumsquad-mysql-schema"
 }
