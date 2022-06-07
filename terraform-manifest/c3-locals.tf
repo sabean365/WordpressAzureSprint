a09d7de529ce99ca5d1e71ac14855bd73d85b875
Sarah Bean <beansarah365@gmail.com>
2022-06-07 11:27:01 -0400

    Delete terraform-manifest directory
    deleting manifest for update

--- terraform-manifest/c3-locals.tf
+++
@@ -1,8 +0,0 @@
-# Define Local Values in Terraform
-locals {
-  owners               = var.business_divsion
-  resource_name_prefix = "scrum-squad-capstone"
-  common_tags = {
-    owners = local.owners
-  }
-} 
